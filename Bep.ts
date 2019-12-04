9c8e674b0f37344274994e88cd402bd733c47dca
Josien94 <56353838+Josien94@users.noreply.github.com>
2019-12-04 16:21:45 +0100

    Update Bep.ts

--- Bep.ts
+++ Bep.ts
@@ -1,14 +1,14 @@
 /**
  * Blocks for driving the Kitronik I2C 16-Servo Driver Board
  */
-//% weight=100 color=#00A654 icon="\uf085" block="BEP"
+//% weight=100 color=#F5992B icon="\uf085" block="Bouw je BEP"
 namespace BEP {
 
 //Some useful parameters. 
     let ChipAddress = 0x6A //default Kitronik Chip address
     let PrescaleReg = 0xFE //the prescale register address
     let Mode1Reg = 0x00  //The mode 1 register address
-
+    
 // If you wanted to write some code that stepped through the servos then this is the BASe and size to do that 	
 	let Servo1RegBase = 0x08 
     let ServoRegDistance = 4
@@ -20,27 +20,27 @@ namespace BEP {
 
     //nice big list of servos for the block to use. These represent register offsets in the PCA9865
     export enum Servos {
-        Servo1 = 0x08,
-        Servo2 = 0x0C,
-        Servo3 = 0x10,
-        Servo4 = 0x14,
-        Servo5 = 0x18,
-        Servo6 = 0x1C,
-        Servo7 = 0x20,
-        Servo8 = 0x24,
-        Servo9 = 0x28,
-        Servo10 = 0x2C,
-        Servo11 = 0x30,
-        Servo12 = 0x34,
-        Servo13 = 0x38,
-        Servo14 = 0x3C,
-        Servo15 = 0x40,
-        Servo16 = 0x44,
+        Rechteroog = 0x08, // Rechteroog is pin 1
+        Linkeroog = 0x0C,		// Linkeroog is pin 2
+        Omhoog_omlaag = 0x10,		// Omhoog of omlaag is pin 3
+        Rechter_wenkbrauw = 0x14,		// Rechter wenkbrauw
+        Linker_wenkbrauw = 0x18,		// Linkerwenkbrauw 
+        Rechter_ooglid = 0x1C,		// Rechter ooglid (Servo6)
+        Linker_ooglid = 0x20,		// Linker ooglied is pin 7 (Servo7)
+        //Servo8 = 0x24,
+        //Servo9 = 0x28,
+        //Servo10 = 0x2C,
+        //Servo11 = 0x30,
+        //Servo12 = 0x34,
+        //Servo13 = 0x38,
+        //Servo14 = 0x3C,
+        //Servo15 = 0x40,
+        //Servo16 = 0x44,
     }
 
 	export enum BoardAddresses{
 		Board1 = 0x6A,
-
+		
 	}
     //Trim the servo pulses. These are here for advanced users, and not exposed to blocks.
     //It appears that servos I've tested are actually expecting 0.5 - 2.5mS pulses, 
@@ -112,8 +112,8 @@ namespace BEP {
         //set the initalised flag so we dont come in here again automatically
         initalised = true
     }
-
-
+	
+	
 /**
      * sets the requested servo to the reguested angle.
 	 * if the PCA has not yet been initialised calls the initialisation routine
@@ -123,25 +123,26 @@ namespace BEP {
      */
     //% blockId=kitronik_I2Cservo_write
     //% block="set%Servo|to%degrees"
-	//% degrees.min=1 degrees.max=5
-
+	//% degrees.min=0 degrees.max=180
+	
     export function servoWrite(Servo: Servos, degrees: number): void {
         if (initalised == false) {
             secretIncantation()
         }
-
+	    
 	// Calculate the right degrees, based on 4 step input: 1 = 15, 2 = 52,  3 = 89, 4 = 126, 5 = 163 with base = 15 degrees, intermdiate steps = 37
 	let choice = degrees
+	let degrees2 = degrees
 	switch(choice){
-		case 1: degrees = 15 ;
-		case 2: degrees = 52;
-		case 3: degrees = 89;
-		case 4: degrees = 126;
-		case 5: degrees = 163;
+		case 1: degrees2 = 15 ;
+		case 2: degrees2 = 52;
+		case 3: degrees2 = 89;
+		case 4: degrees2 = 126;
+		case 5: degrees2 = 163;
 	}
-
+	    
         let buf = pins.createBuffer(2)
-        let HighByte = false
+        let HighByte = false 
         let deg100 = degrees * 100
         let PWMVal100 = deg100 * ServoMultiplier
         let PWMVal = PWMVal100 / 10000
@@ -163,6 +164,6 @@ namespace BEP {
         }
         pins.i2cWriteBuffer(ChipAddress, buf, false)
     }
-
+	    
 
 }
